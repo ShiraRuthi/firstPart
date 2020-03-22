3ff3bdcc7a8c89fbdbd623e96b9d8157fe608e78
Ruthi <Ruthi@DESKTOP-90S9JNE>
2020-03-22 19:28:53 +0200

    commit 1

--- src/geometries/Tube.java
+++ src/geometries/Tube.java
@@ -3,10 +3,25 @@
  */
 package geometries;
 
+import primitives.Point3D;
+import primitives.Vector;
+
 /**
  * @author Ruthi
  *
  */
-public class Tube {
+public class Tube extends RadialGeometry 
+{
+	
+
+	public Tube(RadialGeometry _r) {
+		super(_r);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Vector getNormal(Point3D _p)
+	{
+		return null;
+	}
 
 }
